cdfe6e5825dc1eb6db808ba5f8962a9359117ff0
Lee Moeller <armorgroupsniper03@gmail.com>
2021-11-02 11:55:44 -0400

    assessment 2 for grading

--- module-2/Assessment/blogs/part-2/java/src/main/java/com/techelevator/blog/JdbcPostDao.java
+++ module-2/Assessment/blogs/part-2/java/src/main/java/com/techelevator/blog/JdbcPostDao.java
@@ -1,9 +1,12 @@
 package com.techelevator.blog;
 
+import java.sql.Array;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
@@ -17,11 +20,24 @@ public class JdbcPostDao implements PostDao {
 
 	@Override
 	public void save(Post newPost) {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/assessment");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("postgres1");
+
+		JdbcPostDao application = new JdbcPostDao(dataSource);
+
+
 		// Implement this method to save post to database
 	}
 
 	@Override
 	public List<Post> getAllPosts() {
+		String sql = "SELECT post.id, name, post.body, published, post.created " +
+				"FROM posts";
+		SqlRowSet posts = this.jdbcTemplate.queryForRowSet(sql);
+
+		List<Post> postResult = new ArrayList<>();
 		// Implement this method to pull in all posts from database
 
 		return null;
