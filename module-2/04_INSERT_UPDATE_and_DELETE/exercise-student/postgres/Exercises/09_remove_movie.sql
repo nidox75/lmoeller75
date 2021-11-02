791ab9c5d540d69ef5808160fa99e7cce5f72d05
Lee Moeller <armorgroupsniper03@gmail.com>
2021-11-02 19:55:43 -0400

    MOD 2 INSERT, UPDATE, DELETE for regrading

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/postgres/Exercises/09_remove_movie.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/postgres/Exercises/09_remove_movie.sql
@@ -1,3 +1,16 @@
 -- 9. Remove "Memento" from the movie table
 -- You'll have to remove data from two other tables before you can remove it (13 rows, 2 rows, 1 row)
 
+
+DELETE
+  
+movie_actor.movie_id,
+movie_genre.movie_id,
+movie.movie_id
+ FROM
+  movie
+  JOIN movie_actor ON movie_actor.movie_id = movie.movie_id
+  JOIN movie_genre ON movie_genre.movie_id = movie.movie_id
+ WHERE
+  title = 'Memento';
+  
