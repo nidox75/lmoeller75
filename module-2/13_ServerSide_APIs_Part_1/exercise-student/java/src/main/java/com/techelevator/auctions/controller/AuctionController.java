27e23dc8e027fd6c4538702263708d896bc25604
Lee Moeller <armorgroupsniper03@gmail.com>
2021-11-05 14:52:31 -0400

    mod 2 13 could not get java to load for regrade

--- module-2/13_ServerSide_APIs_Part_1/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_ServerSide_APIs_Part_1/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,11 +2,12 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/auctions")
 public class AuctionController {
+
 
     private AuctionDao dao;
 
@@ -14,4 +15,26 @@ public class AuctionController {
         this.dao = new MemoryAuctionDao();
     }
 
-}
+    @GetMapping(path = "")
+    public List<Auction> list(@RequestParam(defaultValue = "") String title_like, @RequestParam(defaultValue = "0") Double currentBid_lte) {
+        if (!title_like.equals("")) {
+            return dao.searchByTitle(title_like);
+        } else if (currentBid_lte > 0) {
+            return dao.searchByPrice(currentBid_lte);
+        } else {
+            return dao.list();
+
+
+        }
+
+        @GetMapping(path = "/{auctionId}")
+        public Auction get ( @PathVariable int auctionId){
+            return dao.get(auctionId);
+        }
+
+        @PostMapping(path = "")
+        public Auction create (@RequestBody Auction newAuction){
+            return dao.create(newAuction);
+        }
+
+    }
