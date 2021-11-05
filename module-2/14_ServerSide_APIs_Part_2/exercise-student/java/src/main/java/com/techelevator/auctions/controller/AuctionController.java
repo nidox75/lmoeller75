9e2e8be98ab7f0eea2e513969c13c4f70938a8b0
Lee Moeller <armorgroupsniper03@gmail.com>
2021-11-05 15:04:10 -0400

    MOD 2 14 regrade

--- module-2/14_ServerSide_APIs_Part_2/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/14_ServerSide_APIs_Part_2/exercise-student/java/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -3,16 +3,14 @@ package com.techelevator.auctions.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.exception.AuctionNotFoundException;
 import com.techelevator.auctions.model.Auction;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auctions")
@@ -42,9 +40,9 @@ public class AuctionController {
     public Auction get(@PathVariable int id) throws AuctionNotFoundException {
         return dao.get(id);
     }
-
+    @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping( path = "", method = RequestMethod.POST)
-    public Auction create(@RequestBody Auction auction) {
+    public Auction create(@Valid @RequestBody Auction auction) {
         return dao.create(auction);
     }
 
