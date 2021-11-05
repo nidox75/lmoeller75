43789f9ccb8814f0fc6d6f6b97bd55b0dcc2843f
Lee Moeller <armorgroupsniper03@gmail.com>
2021-11-05 13:23:24 -0400

    MOD 1 Assessment for regrading

--- module-1/Assessment/java/src/test/java/HotelReservationTests.java
+++ module-1/Assessment/java/src/test/java/HotelReservationTests.java
@@ -1,10 +1,44 @@
+import com.techelevator.HotelReservation;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class HotelReservationTests {
 
 
     @Test
-    public void Needs_Cleaning_Returns_Price() {
+    public void Test_Estimated_Cost() {
+        HotelReservation hotelReservation = new HotelReservation("test", 10);
 
+        double actual = hotelReservation.getEstimatedTotal();
+
+        Assert.assertEquals(599.90, actual, .5);
+
+
+    }
+
+    @Test
+    public void Test_Minibar_and_Cleaning_Total() {
+        HotelReservation hotelReservation = new HotelReservation("test", 10);
+        double expected = 682.87;
+        double actual = hotelReservation.actualTotal(true, true);
+
+        Assert.assertEquals(expected, actual, .5);
+    }
+
+    @Test
+    public void Test_Minibar_and_No_Cleaning_Total() {
+        HotelReservation hotelReservation = new HotelReservation("test", 10);
+        double expected = 634.89;
+        double actual = hotelReservation.actualTotal(false, true);
+
+        Assert.assertEquals(expected, actual, .5);
+    }
+    @Test
+    public void Test_No_Minibar_and_Yes_Cleaning_Total() {
+        HotelReservation hotelReservation = new HotelReservation("test", 10);
+        double expected = 612.89;
+        double actual = hotelReservation.actualTotal(true, false);
+
+        Assert.assertEquals(expected, actual, .5);
     }
 }
