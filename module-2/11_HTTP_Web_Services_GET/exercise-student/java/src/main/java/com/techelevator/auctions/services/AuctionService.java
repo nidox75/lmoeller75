5c0cf5323ab37776021dec569cbcf2477d21c52b
Lee Moeller <armorgroupsniper03@gmail.com>
2021-11-05 14:14:34 -0400

    mod2 exercise 11 GET for regrade

--- module-2/11_HTTP_Web_Services_GET/exercise-student/java/src/main/java/com/techelevator/auctions/services/AuctionService.java
+++ module-2/11_HTTP_Web_Services_GET/exercise-student/java/src/main/java/com/techelevator/auctions/services/AuctionService.java
@@ -1,8 +1,7 @@
 package com.techelevator.auctions.services;
 
-import org.springframework.web.client.RestTemplate;
-
 import com.techelevator.auctions.model.Auction;
+import org.springframework.web.client.RestTemplate;
 
 public class AuctionService {
 
@@ -11,23 +10,29 @@ public class AuctionService {
 
 
     public Auction[] getAllAuctions() {
-        // call api here
-        return null;
+        String url = API_BASE_URL;
+        Auction[] auctions = restTemplate.getForObject(url, Auction[].class);
+        return auctions;
     }
 
     public Auction getAuction(int id) {
-        // call api here
-        return null;
+        String url = API_BASE_URL + id;
+        Auction auction = restTemplate.getForObject(url, Auction.class);
+        return auction;
     }
 
     public Auction[] getAuctionsMatchingTitle(String title) {
-        // call api here
-        return null;
+        String url = API_BASE_URL + "?title_like=" + title;
+        Auction[] auctions = restTemplate.getForObject(url, Auction[].class);
+        return auctions;
+
     }
 
     public Auction[] getAuctionsAtOrBelowPrice(double price) {
-        // call api here
-        return null;
+        String url = API_BASE_URL +"?currentBid_lte=" + price;
+        Auction[] auctions = restTemplate.getForObject(url, Auction[].class);
+        return auctions;
+
     }
 
 }
