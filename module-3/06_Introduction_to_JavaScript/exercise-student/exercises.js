18d2830cd3d36f56cb1b43cec2f642ac6d4cdeea
Lee Moeller <armorgroupsniper03@gmail.com>
2021-11-28 20:20:42 -0500

    javascript for regrade

--- module-3/06_Introduction_to_JavaScript/exercise-student/exercises.js
+++ module-3/06_Introduction_to_JavaScript/exercise-student/exercises.js
@@ -1,6 +1,6 @@
 ﻿/*
 1. **sumDouble** Given two int values, return their sum. Unless the two values are the 
-    same, then return double their sum.
+	same, then return double their sum.
 
 		sumDouble(1, 2) → 3
 		sumDouble(3, 2) → 5
@@ -10,64 +10,121 @@
 			// do logic here
 			// return result;
 			return x + y;
-        }
+		}
 */
+function sumDouble(param1, param2) {
+	if (param1 != param2) {
+		return param1 + param2;
+	} else {
+		return (param1 + param2) * 2;
+	}
+}
 
 /*
 2. **hasTeen** We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
-    Given 3 int values, return true if 1 or more of them are teen.
+	Given 3 int values, return true if 1 or more of them are teen.
 
 		hasTeen(13, 20, 10) → true
 		hasTeen(20, 19, 10) → true
 		hasTeen(20, 10, 13) → true
 */
+function hasTeen(param1, param2, param3) {
+	const param1IsTeen = (param1 >= 13 && param1 <= 19);
+	const param2IsTeen = (param2 >= 13 && param2 <= 19);
+	const param3IsTeen = (param3 >= 13 && param3 <= 19);
+
+	if (parm1IsTeen) {
+		return true;
+	} else if (param2IsTeen) {
+		return true;
+	} else if (param3IsTeen) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 /* 
 3. **lastDigit** Given two non-negative int values, return true if they have the same 
-    last digit, such as with 27 and 57.
+	last digit, such as with 27 and 57.
 
 		lastDigit(7, 17) → true
 		lastDigit(6, 17) → false
 		lastDigit(3, 113) → true
 */
+function lastDigit(param1, param2) {
+	const param1End = param1 % 10;
+	const param2IsEnd = param2 % 10;
+
+	if (param1End === param2End) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 /*
 4. **seeColor** Given a string, if the string begins with "red" or "blue" return that color 
-    string, otherwise return the empty string.
+	string, otherwise return the empty string.
 
 		seeColor("redxx") → "red"
 		seeColor("xxred") → ""
-        seeColor("blueTimes") → "blue"
+		seeColor("blueTimes") → "blue"
 */
+function seeColor(param1) {
+	if (param1.startsWith("red")) {
+		return "red";
+	}else if (param1.startsWith("blue")) {
+		return "blue";
+	}else {
+		return "";
+	}
+	
+}
 
 /*
-5. **oddOnly** Write a function that given an array of integer of any length, filters out 
-    the even number, and returns a new array of just the the odd numbers.
+5. **oddOnly** Write a function that given an array of integer of any length, filters out
+	the even number, and returns a new array of just the the odd numbers.
 
 		oddOnly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) → [1, 3, 5, 7, 9, 11];
 		oddOnly([2, 4, 8, 32, 256]); → []
 */
+function oddOnly(param1) {
+	return param1.filter(num => % 2 == 1);
+}
 
 /*
-6. **frontAgain** Given a string, return true if the first 2 chars in the string also appear 
-    at the end of the string, such as with "edited".
+6. **frontAgain** Given a string, return true if the first 2 chars in the string also appear
+	at the end of the string, such as with "edited".
 
 		frontAgain("edited") → true
 		frontAgain("edit") → false
 		frontAgain("ed") → true
 */
+function frontAgain(param1) {
+start = param1.substring(0, 2);
+	end = param1.substring(param1.length - 2);
+
+	if (start == end) {
+		return true;
+	} else {
+		return false;
+	}
 
 /*
-7. **cigarParty** When squirrels get together for a party, they like to have cigars. 
-A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. 
-Unless it is the weekend, in which case there is no upper bound on the number of cigars. 
-Write a squirrel party function that return true if the party with the given values is successful, 
+7. **cigarParty** When squirrels get together for a party, they like to have cigars.
+A squirrel party is successful when the number of cigars is between 40 and 60, inclusive.
+Unless it is the weekend, in which case there is no upper bound on the number of cigars.
+Write a squirrel party function that return true if the party with the given values is successful,
 or false otherwise.
 
 		cigarParty(30, false) → false
 		cigarParty(50, false) → true
 		cigarParty(70, true) → true
 */
+function cigarParty(param1, param2) {
+	return param1 >=40 && ((param1 <= 60 && param2 == false) || (param2 == true));
+}
 
 /*
 8. **fizzBuzz** Because you know you can't live without it, FizzBuzz.
@@ -78,6 +135,20 @@ or false otherwise.
 		fizzBuzz(15) → "FizzBuzz"
 		fizzBuzz(8) → 8
 */
+function fizzBuzz(param1) {
+	answer="";
+
+	if (param1 % 3 == 0 && param1 % 5 == 0) {
+		answer= "FizzBuzz";
+	} else if (param1 % 5 == 0) {
+		
+	}else if (param1 % 3 == 0) {
+		answer = "Fizz";
+	}else {
+		answer = param1;
+	}
+	return answer;
+}
 
 /*
 9. **filterEvens** Write a function that filters an array to only include even numbers.
@@ -87,6 +158,9 @@ or false otherwise.
 	filterEvens([2, 4, 6]) → [2, 4, 6]
 	filterEvens([100, 8, 21, 24, 62, 9, 7]) → [100, 8, 24, 62]
 */
+function filterEvens(param1) {
+	return param.filter(nums => nums % 2 == 0);
+}
 
 /*
 10. **filterBigNumbers** Write a function that filters numbers greater than or equal to 100.
@@ -95,17 +169,23 @@ or false otherwise.
 	filterBigNumbers([3, 2, 7, 1, -100, -120]) → []
 	filterBigNumbers([]) → []
 */
+function filterBigNumbers(param1) {
+	return param.filter(nums => nums >= 100);
+}
 
 /*
-11. **filterMultiplesOfX** Write a function to filter numbers that are a multiple of a 
+11. **filterMultiplesOfX** Write a function to filter numbers that are a multiple of a
 parameter, `x` passed in.
 
 	filterMultiplesOfX([3, 5, 1, 9, 18, 21, 42, 67], 3) → [3, 9, 18, 21, 42]
 	filterMultiplesOfX([3, 5, 10, 20, 18, 21, 42, 67], 5) → [5, 10, 20]
 */
+function filterMultiplesOfX(param1, param2) {
+	return param1.filter(nums => nums % param2 == 0 );
+}
 
 /*
-12. **createObject** Write a function that creates an object with a property called 
+12. **createObject** Write a function that creates an object with a property called
 firstName, lastName, and age. Populate the properties with your values.
 
 	createObject() →
@@ -116,3 +196,11 @@ firstName, lastName, and age. Populate the properties with your values.
 		age
 	}
 */
+function createObject() {
+	const person = {
+		firstName = "Lee",
+		lastName = "Moeller",
+		age: 46
+	}
+	return person;
+}
