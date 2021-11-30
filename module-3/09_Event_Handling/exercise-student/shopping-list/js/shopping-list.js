d79e0ad09fe700884c33d3d3780886b0748d99a8
Lee Moeller <armorgroupsniper03@gmail.com>
2021-11-30 06:45:02 -0500

    mod 3 9 fixed for regrade

--- module-3/09_Event_Handling/exercise-student/shopping-list/js/shopping-list.js
+++ module-3/09_Event_Handling/exercise-student/shopping-list/js/shopping-list.js
@@ -1,9 +1,4 @@
 
-<li v-for="item in groceries" v-bind: key="item.id" v-bind: class="{completed: item.completed}"
-
-@click="changeStatus(item.id,$event)" >
-  <input type="checkbox" />
-
 let allItemsIncomplete = true;
 const pageTitle = 'My Shopping List';
 const groceries = [
@@ -26,7 +21,6 @@ const groceries = [
 function setPageTitle() {
   const title = document.getElementById('title');
   title.innerText = pageTitle;
-  shoppingList.appendChild(heading);
 }
 
 /**
@@ -43,39 +37,55 @@ function displayGroceries() {
     ul.appendChild(li);
   });
 }
-document.addEventListener("DOMContentLoaded", () => {
-  IntersectionObserver();
-  addPageTitle();
-  addShoppingList;
-
-  const tasks = document.querySelectorAll(li);
-
-  tasks.forEach((task)) => {
-
-    task.addEventListener('click', () => {
-      if (!task, classList.contains('completed')) {
-        task.classList.add('completed');
-        task.querySelector('i').classList.a
-        add('completed');
-      }
-    });
-
-    task.addEventListener('dblclick', () => {
-      if (task.classList.contains('completed')) {
-        task.classList.remove('completed');
-        task.querySelector('i').classList.remove('completed')
-      }
-    });
-    const completeAll = document.getElementById('btnCompleteAll');
-    completeAll.addEventListener('click', () => {
-      tasks.forEach((task) => {
-        task.completeAll.add('completed');
-        task.querySelector('i').classList.add('completed');
-      });
-    });
-
-  }
-});
 
 setPageTitle();
 displayGroceries();
+
+
+
+document.addEventListener('DOMContentLoaded', () => 
+{
+  // Register all of your event listeners here
+  
+  const listItem = document.querySelectorAll("li")
+  const checkCircle = document.querySelectorAll(".far")
+
+  for (let i = 0; i < listItem.length; i++) {
+    listItem[i].addEventListener('click', () => {
+      const element = listItem[i]; 
+      if (listItem[i].classList.contains('completed') === false){
+      listItem[i].classList.add('completed');
+      checkCircle[i].classList.add('completed');}
+    })
+  }
+
+  for (let i = 0; i < listItem.length; i++) {
+    listItem[i].addEventListener('dblclick', () => {
+      const element = listItem[i]; 
+      if (listItem[i].classList.contains('completed') === true){  
+      listItem[i].classList.remove('completed');
+      checkCircle[i].classList.remove('completed');}
+    })
+  }
+ 
+  const button = document.querySelector('.btn')
+ 
+  //mark all as complete
+  button.addEventListener('click', () => {
+    if (allItemsIncomplete === true) {
+      for (let i = 0; i < listItem.length; i++) {
+        listItem[i].classList.add('completed');
+        checkCircle[i].classList.add('completed');
+      }
+      allItemsIncomplete = false;
+      button.innerText = "Mark All Incomplete";
+    } else {
+      for (let i = 0; i < listItem.length; ++i) {
+        listItem[i].classList.remove('completed');
+        checkCircle[i].classList.remove('completed');
+      }
+      allItemsIncomplete = true;
+      button.innerText = "Mark All Complete";
+    }
+  })
+});
