6e71b5b773ee2057e8f9ca3e4c7965650c58c1ad
Lee Moeller <armorgroupsniper03@gmail.com>
2021-12-15 13:21:20 -0500

    16 added services folder with Topics.js

---
+++ module-3/16_Web_Services_GET/exercise-student/src/Services/TopicsService.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: "http://localhost:3000"
+});
+
+export default {
+
+  getTopics() {
+    return http.get('/topics');
+  },
+  getTopicById(topicId){
+      return http.get(`/topics/${topicId}`);
+
+  }
+
+
+}
